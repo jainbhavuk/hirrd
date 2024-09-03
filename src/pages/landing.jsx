9bca5e75ebba845717b5e8712a5f87f3ee6b5446
jainbhavuk <bhavukspeaks@gmail.com>
2024-09-03 18:07:01 +0530

    fix carousel negative z index

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -53,7 +53,7 @@ const LandingPage = () => {
             delay: 2000,
           }),
         ]}
-        className="w-full py-10"
+        className="w-full py-10 -z-10"
       >
         <CarouselContent className="flex gap-5 sm:gap-20 items-center">
           {companies.map(({ name, id, path }) => (
